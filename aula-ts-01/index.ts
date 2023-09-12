15cee60ab7856564a0253eb8005e8abe9ad951ef
Kess220 <prodvictorkaio@gmail.com>
2023-09-12 15:30:41 -0300

    aula2

--- aula-ts-01/index.ts
+++ aula-ts-01/index.ts
@@ -2,5 +2,5 @@ function somar(num1: number, num2: number) {
   return num1 + num2;
 }
 
-const resultado = somar(3, 3);
+const resultado = somar(2, 3);
 console.log(resultado);
---
+++ aula-ts-02/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function somar(num1, num2) {
+    return num1 + num2;
+}
+const resultado = somar(2, 3);
+console.log(resultado);
