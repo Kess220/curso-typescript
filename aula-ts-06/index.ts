97d73ac1aa259bb55652c833d885f6008f24cbe0
Kess220 <prodvictorkaio@gmail.com>
2023-09-13 18:20:01 -0300

    aula-06

---
+++ aula-ts-06/index.ts
@@ -0,0 +1,34 @@
+const game: {
+  id: number;
+  platform: {
+    id: number;
+    name: string;
+  };
+  title: string;
+  publisher: string;
+  launch?: string;
+} = {
+  id: 1,
+  platform: {
+    id: 1,
+    name: "Playstation",
+  },
+  title: "The Last of Us",
+  publisher: "Naughty Dog",
+  launch: "2013-06-14", // pode ser um Date. É opcional (para jogos ainda não lançados).
+};
+
+const games = [game];
+
+function play(game: {
+  id: number;
+  platform: {
+    id: number;
+    name: string;
+  };
+  title: string;
+  publisher: string;
+  launch?: string;
+}): void {
+  console.log(game);
+}
